6814d79a515ce710bf1fd56335d532fec205517a
slashman <java.koder@gmail.com>
2016-02-18 15:04:33 -0500

    Hide seal on click

--- psoa.js
+++ psoa.js
@@ -8,6 +8,7 @@ function show(){
 	seal.src = "http://slashman.github.io/programmers-seal-of-approval/seal.png";
 	sealContainer.appendChild(seal);
 	document.getElementsByTagName("body")[0].appendChild(sealContainer);
+	sealContainer.addEventListener("click", function(){this.style.display = "none";});
 }
 
 window.addEventListener("load", show);
